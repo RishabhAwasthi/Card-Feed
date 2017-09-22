5e3c91689b1daa8382422441b759ea77e4911f33
rishabh <rishabh7awashi@gmail.com>
2017-09-23 01:21:26 +0530

    no toolbar

---
+++ app/src/main/java/com/delaroystudios/carddemo/CardDemoActivity.java
@@ -0,0 +1,55 @@
+package com.delaroystudios.carddemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CardDemoActivity extends AppCompatActivity {
+    Toolbar tool_bar;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_demo);
+        tool_bar = (Toolbar) findViewById(R.id.toolbar);
+        tool_bar.setTitle("Deets");
+        setSupportActionBar(tool_bar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    /*@Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_card_demo, menu);
+        return true;
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+       // if (id == R.id.action_settings) {
+         //   return true;
+        //}
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/delaroystudios/carddemo/RecyclerAdapter.java
@@ -0,0 +1,87 @@
+package com.delaroystudios.carddemo;
+
+/**
+ * Created by Shade on 5/9/2016.
+ */
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private String[] titles = {"How do we expand business?",
+            "How is branding different from marketing?",
+            "How did u start business",
+            "Who is the best business ever?",
+            "Strange business strategies?",
+            "Mind blowing decisions?",
+            "How did you start at amazon?",
+            "Experience at amazon?",};
+
+    private String[] details = {"It is tricky at times to expand a business..",
+            "Branding is quality assurance, whereas..", "If I start shelling out strategies soon..",
+            "Meet Ram, he has a turnover of ru..", "I saw a person doing weird advertising..",
+            "A decision I took, turned the tides..", "Now when I see amazon, I feel like..",
+            "Woah! way better than westros.."};
+
+    private int[] images = { R.drawable.jorah,
+            R.drawable.dany,
+            R.drawable.arya,
+            R.drawable.tyrion,
+            R.drawable.little,
+            R.drawable.jon,
+            R.drawable.night,
+            R.drawable.jaime };
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public int currentItem;
+        public ImageView itemImage;
+        public TextView itemTitle;
+        public TextView itemDetail;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemImage = (ImageView)itemView.findViewById(R.id.item_image);
+            itemTitle = (TextView)itemView.findViewById(R.id.item_title);
+            itemDetail =
+                    (TextView)itemView.findViewById(R.id.item_detail);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Snackbar.make(v, "Click detected on item " + position,
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.card_layout, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        viewHolder.itemTitle.setText(titles[i]);
+        viewHolder.itemDetail.setText(details[i]);
+        viewHolder.itemImage.setImageResource(images[i]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length;
+    }
+}
